d7ff2d68addbee5429b934e65d06f7555a6bbc77
pito <60766670+trujan7@users.noreply.github.com>
2024-06-23 22:47:29 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -91,7 +91,6 @@
 	    animateOut: 'fadeOut',
 	    animateIn: 'fadeIn',
 	    nav:false,
-		touchDrag: false,
 	    autoplayHoverPause: false,
 	    items: 1,
 	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
