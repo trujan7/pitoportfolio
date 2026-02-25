955de554efe8ead03ee02d08807197dc1e927dac
pito <peterudtujan@gmail.com>
2026-02-25 19:45:54 +0800

    Updated config for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/pitoportfolio",
+  images: {
+    unoptimized: true,
+  },
   sassOptions: {
     compiler: "modern",
     silenceDeprecations: ["legacy-js-api"],
